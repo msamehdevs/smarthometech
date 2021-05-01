653bac3ce5015bfd09dc1c213966681ff30bb673
unknown <msamehvs@outlook.com>
2021-05-02 01:51:40 +0200

    test

--- smarthome/settings.py
+++ smarthome/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'xc(=vgmw2vdun%*_up^-ar4^150f(#y7ui()(*b+6^em-a@(v^'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
